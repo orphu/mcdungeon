1870b18c241fbd8aa951ffeff1b50635041118e3
orphu <github@bubblemod.org>
2011-03-31 17:29:15 -0400

    Version bump.

--- mcdungeon.py
+++ mcdungeon.py
@@ -3,7 +3,7 @@
 import sys
 import argparse
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __version_info__ = tuple([ int(num) for num in __version__.split('.')])
 _vstring = '%%(prog)s %s' % (__version__)
 
