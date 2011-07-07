9ea778851d292bf6cfe826baf932ca3f907176e9
orphu <github@bubblemod.org>
2011-07-07 14:14:21 -0400

    Quick test script for exploring perlin parameters.

---
+++ perlin_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+size = 16
+factor = float(size) * 0.25
+i = [' ', '.', ',', 'o', 'O', '@', '#']
+import perlin
+pn = perlin.SimplexNoise(256)
+for y in xrange(size):
+    for x in xrange(size):
+        print i[int((pn.noise3(x/factor,0,y/factor) + 1.0) / 2 * 6)],
+    print
+
