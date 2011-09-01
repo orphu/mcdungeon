631102c1e794c2922e787b8c92dbef6dafec1d40
orphu <github@bubblemod.org>
2011-09-01 10:49:46 -0400

    Bump to 0.5.3

--- mcdungeon.py
+++ mcdungeon.py
@@ -12,7 +12,7 @@ from pymclevel import mclevel, nbt
 import pmeter
 
 # Version info
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 __version_info__ = tuple([ num for num in __version__.split('.')])
 _vstring = '%%(prog)s %s' % (__version__)
 
