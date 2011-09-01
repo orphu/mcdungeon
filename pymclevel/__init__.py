66d0ad58ba10980df1b8b95fef22958f08bc74f7
orphu <github@bubblemod.org>
2011-09-01 11:11:38 -0400

    Converting pymclevel to a submodule...

--- pymclevel/__init__.py
+++
@@ -1,9 +0,0 @@
-from mclevel import fromFile, loadWorld, loadWorldNumber, saveFileDir, minecraftDir
-from indev import *
-from infiniteworld import *
-from java import *
-from level import *
-from schematic import *
-from materials import *
-
-import items
