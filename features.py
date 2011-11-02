6da05ac8451728904a8df5ee42945950908d66a6
orphu <github@bubblemod.org>
2011-11-01 23:38:45 -0400

    Fixed secret rooms.

--- features.py
+++ features.py
@@ -123,71 +123,6 @@ class Stairwell(Blank):
                     self.parent.parent.setblock(p.trans(0,1,0),
                                                 materials.meta_mossycobble)
 
-class TreasureRoom(Blank):
-    _name = 'treasureroom'
-
-    def render (self):
-        if (sum_points_inside_flat_poly(*self.parent.canvas) > 24):
-            center = self.parent.loc.trans(self.parent.parent.room_size/2,
-                                           self.parent.parent.room_height-2,
-                                           self.parent.parent.room_size/2)
-            # Fix the floor. Clear an area 
-            for p in iterate_disc(center.trans(-4, 0, 3),
-                                  center.trans(3, 0, -4)):
-                self.parent.parent.setblock(p, materials.Air)
-            for p in iterate_ellipse(center.trans(-4, 0, 3),
-                                  center.trans(3, 0, -4)):
-                self.parent.parent.setblock(p, materials.StoneSlab)
-            # Treasure!
-            self.parent.parent.setblock(center,
-                                        materials.Chest)
-            self.parent.parent.addchest(center, 
-                                        loottable._maxtier)
-
-class MultiVersePortal(Blank):
-    _name = 'multiverseportal'
-    target = 'World1'
-
-    def render (self):
-        if (sum_points_inside_flat_poly(*self.parent.canvas) > 24):
-            center = self.parent.loc.trans(self.parent.parent.room_size/2,
-                                           self.parent.parent.room_height-2,
-                                           self.parent.parent.room_size/2)
-            # Fix the floor. Clear an area for the portal.
-            for p in iterate_disc(center.trans(-4, 0, 3),
-                                  center.trans(3, 0, -4)):
-                self.parent.parent.setblock(p, materials.Air)
-            for p in iterate_ellipse(center.trans(-4, 0, 3),
-                                  center.trans(3, 0, -4)):
-                self.parent.parent.setblock(p, materials.StoneSlab)
-            # Obsidian portal frame.
-            for p in iterate_cube(center.trans(-2,1,0), center.trans(1,-3,0)):
-                self.parent.parent.setblock(p, materials.Obsidian)
-            # Portal stuff.
-            for p in iterate_cube(center.trans(-1,0,0), center.trans(0,-2,0)):
-                self.parent.parent.setblock(p, materials.NetherPortal)
-            # Signs.
-            self.parent.parent.setblock(center.trans(1,-1,-1),
-                                        materials.WallSign)
-            self.parent.parent.blocks[center.trans(1,-1,-1)].data = 3
-            self.parent.parent.setblock(center.trans(-2,-1,1),
-                                        materials.WallSign)
-            self.parent.parent.blocks[center.trans(-2,-1,1)].data = 2
-            # Create the tile entities for the signs.
-            self.parent.parent.addsign(center.trans(1,-1,-1),
-                                       '[multiverse]',
-                                       self.target,
-                                       '','')
-            self.parent.parent.addsign(center.trans(-2,-1,1),
-                                       '[multiverse]',
-                                       self.target,
-                                       '', '')
-            # Treasure!
-            self.parent.parent.setblock(center.trans(-2,0,-3),
-                                        materials.Chest)
-            self.parent.parent.addchest(center.trans(-2,0,-3),
-                                        loottable._maxtier)
-
 
 class Chasm(Blank):
     _name = 'chasm'
@@ -480,7 +415,7 @@ class SecretRoom(Blank):
             materials.WoodenStairs  # 5
         ]
         template = [
-            [2,1,1,3],
+            [3,1,1,2],
             [0,4,5,4]
         ]
         oo = o.trans(5,3,4)
@@ -571,7 +506,7 @@ class SecretRoom(Blank):
             [materials.Stone,0],        # 3
             [materials._wall,0],        # 4
             [materials.RedStoneWire,0], # 5
-            [materials.StickyPiston,2], # 6 - toggle piston
+            [materials.StickyPiston,3], # 6 - toggle piston
             [materials.RedStoneTorchOn,2],# 7
             [materials._ceiling, 0],    # 8
             [materials.StickyPiston, 4],# 9 - pusher piston
@@ -614,22 +549,22 @@ class SecretRoom(Blank):
             [15,15,15,15,15],
             [15,15,15,15,15]],
         ]
-        bdata = 4
+        bdata = 3
 
         # Data adjustments for directions. There are 8, but the defaults are
         # for East, low offset. 
 
         # West
         if d == 0:
-            bdata = 3
+            bdata = 4
             if offset >= 7:
-                mats[6][1] = 3
+                mats[6][1] = 2
                 mats[7][1] = 1
                 mats[9][1] = 5
                 mats[10][1] = 1
                 mats[11][1] = 5
             else:
-                mats[6][1] = 3
+                mats[6][1] = 2
 
         # South
         if d == 1:
@@ -637,13 +572,13 @@ class SecretRoom(Blank):
             if offset >= 7:
                 mats[6][1] = 5
                 mats[7][1] = 4
-                mats[9][1] = 2
+                mats[9][1] = 3
                 mats[10][1] = 0
                 mats[11][1] = 4
             else:
                 mats[6][1] = 5
                 mats[7][1] = 3
-                mats[9][1] = 3
+                mats[9][1] = 2
                 mats[10][1] = 2
                 mats[11][1] = 6
 
@@ -661,13 +596,13 @@ class SecretRoom(Blank):
             if offset >= 7:
                 mats[6][1] = 4
                 mats[7][1] = 4
-                mats[9][1] = 2
+                mats[9][1] = 3
                 mats[10][1] = 0
                 mats[11][1] = 4
             else:
                 mats[6][1] = 4
                 mats[7][1] = 3
-                mats[9][1] = 3
+                mats[9][1] = 2
                 mats[10][1] = 2
                 mats[11][1] = 6
 
