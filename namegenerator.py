de2173d75a4358ca2a5cbfd63c437a2ac18cdb1d
JiFish <jifish0@gmail.com>
2013-03-01 01:28:27 +0000

    Implement orphu's suggestions to namegenerator
    Issue #104

--- namegenerator.py
+++ namegenerator.py
@@ -1,44 +1,41 @@
 import os
 import sys
 import random
+from utils import weighted_choice
 from namegen import namegen
 
 class namegenerator:
     def __init__(self, biomeid):
-        # Choose DICT based on biome
-        if (biomeid == 2 or     # Desert
-            biomeid == 17):     # DesertHills
+        # Get theme from biome id
+        theme = self.gettheme(biomeid)
+        print theme
+        # Choose DICT based on theme
+        if (theme == 'egyptian'):
             dict = 'egyptian.txt'
-            self.titles = ['Pharaoh']
-        elif (biomeid == 5 or   # Taiga
-              biomeid == 10 or  # FrozenOcean
-              biomeid == 12 or  # Ice Plains
-              biomeid == 13 or  # Ice Mountains
-              biomeid == 19):   # TaigaHills
+            self.titles = ['Pharaoh','High Priest','Vizier']
+        elif (theme == 'norse'):
             dict = 'norse.txt'
-            self.titles = ['King','Queen']
-        elif (biomeid == 6):    # Swampland
+            self.titles = ['King','Queen','Jarl','Hojfruen']
+        elif (theme == 'saxon'):
             dict = 'saxon.txt'
-            self.titles = ['King','Queen']
-        elif (biomeid == 4 or   # Forest
-              biomeid == 18):   # ForestHills
+            self.titles = ['King','Queen', 'Elder']
+        elif (theme == 'elven'):
             dict = 'elven.txt'
-            self.titles = ['King','Queen']
-        elif (biomeid == 21 or  # Jungle
-              biomeid == 22):   # Jungle Hills
+            self.titles = ['King','Queen','Lord','Lady']
+        elif (theme == 'mayan'):
             dict = 'mayan.txt'
-            self.titles = ['Tepal']
-        elif (biomeid == 14 or  # MushroomIsland
-              biomeid == 15):   # MushroomIslandShore
+            self.titles = ['Tepal','Ajaw']
+        elif (theme == 'welsh'):
             dict = 'welsh.txt'
             self.titles = ['Y Brenin','Y Frenhines']
-        elif (biomeid == 3 or   # ExtremeHills
-              biomeid == 20):   # ExteremeHillsEdge
+        elif (theme == 'greek'):
             dict = 'greek.txt'
-            self.titles = ['King','Queen']
-        else:                   # The rest. Including plains, rivers,
-            dict = 'roman.txt'  # oceans and any misc biomes
-            self.titles = ['Emperor','Empress']
+            self.titles = ['King','Queen','Strategos',
+                           'Tyrant','Archon']
+        elif (theme == 'roman'):
+            dict = 'roman.txt'
+            self.titles = ['Emperor','Empress', 'Praetor',
+                           'Prefect', 'Consul', 'Magister']
 
         # Find name dictionaries
         if os.path.isdir(os.path.join(sys.path[0],'names')):
@@ -50,6 +47,48 @@ class namegenerator:
 
         # Set up name generator
         self.generator = namegen.NameGen(os.path.join(names_path,dict))
+
+    def gettheme(self, biomeid):
+        # Choose DICT based on biome
+        if (biomeid == 2 or     # Desert
+            biomeid == 17):     # DesertHills
+            theme_weights = [('egyptian', 50),
+                             ('greek', 10),
+                             ('roman', 10)]
+        elif (biomeid == 5 or   # Taiga
+              biomeid == 10 or  # FrozenOcean
+              biomeid == 12 or  # Ice Plains
+              biomeid == 13 or  # Ice Mountains
+              biomeid == 19):   # TaigaHills
+            theme_weights = [('norse', 50),
+                             ('elven', 10),
+                             ('saxon', 10)]
+        elif (biomeid == 6 or   # Swampland
+              biomeid == 3 or   # ExtremeHills
+              biomeid == 20 or  # ExteremeHillsEdge
+              biomeid == 1):    # Plains
+            theme_weights = [('saxon', 20),
+                             ('roman', 10),
+                             ('greek', 10)]
+        elif (biomeid == 4 or   # Forest
+              biomeid == 18):   # ForestHills
+            theme_weights = [('elven', 50),
+                             ('roman', 10),
+                             ('norse', 10)]
+        elif (biomeid == 21 or  # Jungle
+              biomeid == 22):   # Jungle Hills
+            theme_weights = [('mayan', 60),
+                             ('elven', 10)]
+        elif (biomeid == 14 or  # MushroomIsland
+              biomeid == 15):   # MushroomIslandShore
+            theme_weights = [('welsh', 1)]
+        else:                   # The rest. Including rivers, oceans etc
+            theme_weights = [('saxon', 10),
+                             ('roman', 10),
+                             ('greek', 10),
+                             ('norse', 10),
+                             ('elven', 10)]
+        return weighted_choice(theme_weights)
 
     def genname(self):
         name = self.generator.gen_word(True)    # True indicates a unqiue name
