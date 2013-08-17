5cdcc53564915f7e9b5795672b33a7d602d693c4
orphu <github@bubblemod.org>
2013-08-17 14:05:42 -0400

    Fix for crash on unknown biomes. Closes#171

--- dungeon.py
+++ dungeon.py
@@ -1511,8 +1511,12 @@ class Dungeon (object):
             if (pos.y == 0 and
                 len(self.rooms[pos].ruins) == 0):
                 if pos == self.entrance.parent.pos:
-                    ruin = ruins.new(weighted_choice(cfg.master_entrances[self.biome]),
-                                     self.rooms[pos])
+                    try:
+                        ruin = ruins.new(weighted_choice(cfg.master_entrances[self.biome]),
+                                         self.rooms[pos])
+                    except KeyError:
+                        ruin = ruins.new(weighted_choice(cfg.default_entrances),
+                                         self.rooms[pos])
                     self.dinfo['dungeon_name'] = ruin.nameDungeon()
                 else:
                     ruin = ruins.new(weighted_choice(cfg.master_ruins),
