6c78a812d8b5e8705b5694fa01a30e8fd10effc6
orphu <github@bubblemod.org>
2013-09-08 12:44:56 -0400

    Added 1.7 biomes to name generator themes.
    Issue #176

--- namegenerator.py
+++ namegenerator.py
@@ -49,37 +49,80 @@ class namegenerator:
 
     def gettheme(self, biomeid):
         # Choose DICT based on biome
-        if (biomeid == 2 or     # Desert
-            biomeid == 17):     # DesertHills
+        if (biomeid in (2,      # Desert
+                        130,    # Desert M
+                        17,     # DesertHills
+                        35,     # Savanna
+                        163,    # Savanna M
+                        36,     # Savanna Plateau
+                        164,    # Savanna Plateau M
+                        37,     # Mesa
+                        165,    # Mesa (Bryce)
+                        38,     # Mesa Plateau F
+                        166,    # Mesa Plateau F M
+                        39,     # Mesa Plateau
+                        167,    # Mesa Plateau M
+                       )):
             theme_weights = [('egyptian', 50),
                              ('greek', 10),
                              ('roman', 10)]
-        elif (biomeid == 5 or   # Taiga
-              biomeid == 10 or  # FrozenOcean
-              biomeid == 12 or  # Ice Plains
-              biomeid == 13 or  # Ice Mountains
-              biomeid == 19):   # TaigaHills
+        elif (biomeid in (5,    # Taiga
+                          133,  # Taiga M
+                          10,   # FrozenOcean
+                          12,   # Ice Plains
+                          140,  # Ice Plains Spikes
+                          13,   # Ice Mountains
+                          19,   # TaigaHills
+                          26,   # Cold Beach
+                          30,   # Cold Taiga
+                          158,  # Cold Taiga M
+                          31,   # Cold Taiga Hills
+                          32,   # Mega Taiga
+                          160,  # Mega Spruce Taiga
+                          33,   # Mega Taiga Hills
+                          161,  # Mega Spruce Taiga
+                         )):
             theme_weights = [('norse', 50),
                              ('elven', 10),
                              ('saxon', 10)]
-        elif (biomeid == 6 or   # Swampland
-              biomeid == 3 or   # ExtremeHills
-              biomeid == 20 or  # ExteremeHillsEdge
-              biomeid == 1):    # Plains
+        elif (biomeid in (1,    # Plains
+                          129,  # Sunflower Plains
+                          3,    # Extreme Hills
+                          131,  # Extreme Hills M
+                          6,    # Swampland
+                          134,  # Swampland M
+                          20,   # Extreme Hills Edge
+                          26,   # Stone Beach
+                          34,   # Extreme Hills+
+                          162,  # Extreme Hills+ M
+                         )):
             theme_weights = [('saxon', 20),
                              ('roman', 10),
                              ('greek', 10)]
-        elif (biomeid == 4 or   # Forest
-              biomeid == 18):   # ForestHills
+        elif (biomeid in(4,     # Forest
+                         132,   # Flower Forest
+                         18,    # ForestHills
+                         27,    # Birch Forest
+                         155,   # Birch Forest M
+                         28,    # Birch Forest Hills
+                         156,   # Birch Forest Hills M
+                         29,    # Roofed Forest
+                         157,   # Roofed Forest M
+                        )):
             theme_weights = [('elven', 50),
                              ('roman', 10),
                              ('norse', 10)]
-        elif (biomeid == 21 or  # Jungle
-              biomeid == 22):   # Jungle Hills
+        elif (biomeid in (21,   # Jungle
+                          149,  # Jungle M
+                          22,   # JungleHills
+                          23,   # JungleEdge
+                          151,  # JungleEdge M
+                         )):
             theme_weights = [('mayan', 60),
                              ('elven', 10)]
-        elif (biomeid == 14 or  # MushroomIsland
-              biomeid == 15):   # MushroomIslandShore
+        elif (biomeid in (14,   # MushroomIsland
+                          15,   # MushroomIslandShore
+                         )):
             theme_weights = [('welsh', 1)]
         else:                   # The rest. Including rivers, oceans etc
             theme_weights = [('saxon', 10),
