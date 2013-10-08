orphu/mcdungeon

commit bf69e750050a1d1cc121c931dab3d2e72f293bac

Add sample 'themed' configuration
Add sample 'themed' configuration. Issue #164
Also minor fixes to mapstore directory config

mapstore.py
@@ -25,7 +25,7 @@ from utils import Vec
 import materials
 
 class new:
-    def __init__(self, mapstore):
+    def __init__(self, mapstore, dir_paintings):
         self.mapstore = os.path.join(mapstore, 'data')
 
         # Load the idcounts.dat NBT if it exists, otherwise make
@@ -58,10 +58,10 @@ class new:
                 self.maphash[hash] = int(file[4:-4])
 
         # Store paintings path
-        if os.path.isdir(os.path.join(sys.path[0],'paintings')):
-            self.painting_path = os.path.join(sys.path[0],'paintings')
-        elif os.path.isdir('paintings'):
-            self.painting_path = 'paintings'
+        if os.path.isdir(os.path.join(sys.path[0],dir_paintings)):
+            self.painting_path = os.path.join(sys.path[0],dir_paintings)
+        elif os.path.isdir(dir_paintings):
+            self.painting_path = dir_paintings
         else:
             sys.exit("Error: Could not find the paintings folder!")
 

configs/sample_themed.cfg
@@ -1,0 +1,188 @@
+# MCDungeon configuration file
+#
+# An example of using the locations section.
+# This configuration won't generate intresting dungeons, it's just an example.
+
+# The [locations] section and it's values are optional. They are used to
+# specify the locations of files and directories used by MCDungeon. This
+# allows you to use a given set of resources with the config.
+#
+# Paths are relative to config folder. It's recommended you place them in
+# subfolders to keep everything organised. e.g.
+# 
+# configs
+#    |-> my_config.cfg
+#        my_config\
+#            |-> fortunes.txt
+#                books\
+
+[locations]
+# Load item IDs in additon to those specified in items.txt
+# This file is ideal for adding mod items for loot.
+# See item.txt for the format
+file_extra_items: sample_themed\items.txt
+
+# Load dyed armour colors from this file. See dye_colors.txt for the format.
+file_dyes: sample_themed\dyes.txt
+
+# Load custom potions from this file. See potions.txt for the format.
+file_potions: sample_themed\potions.txt
+
+# Load magic (enchanted/lore) items from this file.
+# See magic_items.txt for the format
+file_magic_items: sample_themed\magic_items.txt
+
+# Load the fortunes from this file. See fortunes.txt for the format.
+file_fortunes: sample_themed\insults.txt
+
+# Choose custom paintings from this directory.
+# See README.txt in the paintings folder for more information
+dir_paintings: sample_themed\fractals
+
+# Choose written paintings from this directory.
+# See README.txt in the books folder for more information
+dir_books: sample_themed\books
+
+# Additionally load spawner tag files from this directory.
+# (Spawners in the normal directory can still be loaded.)
+# See README.txt in the spawners folder for more information
+dir_extra_spawners: sample_themed\spawners
+
+# Additionally load item tag files from this directory.
+# (Items in the normal directory can still be loaded.)
+# See README.txt in the items folder for more information
+dir_extra_items: sample_themed\items
+
+#
+# Now we can use those resources in our configuration
+#
+[dungeon]
+spawners: 10
+
+# Use an item from the item folder and a in a trap
+chest_traps: 75
+arrow_traps: 10
+[chest_traps]
+file_broken_firework: 1, 1
+[dispensers]
+file_broken_firework: 1, 10
+
+# Use custom mob spawners from the extra spawners directory
+[mobs.0]
+jifish: 10
+#orphu: 10
+Zombie: 10
+Zombie_Fast: 10
+
+
+# Loot chests with example items from resources
+[tier0]
+# From extra items.txt
+smooth full halfslab: 100,1,0
+# From specified dye file
+Hot Pink Leather Chestplate: 100,1,0
+# From specfied potion file
+Energy Drink: 100,1,0
+# From specified magic items file
+magic_Quantum Gemerald: 100,1,0
+# Fortune cookie, getting it's fortunes from specified file
+Fortune Cookie: 100,1,0
+# Custom painting, choosing a random painting from the specified directory
+Custom Painting: 100,1,0
+# Written book, choosing a random text from the specified directory
+Written Book: 100,1,0
+# From an item tag file in the specified directory
+file_head_herobrine: 100,1,0
+
+[tier1]
+smooth full halfslab: 100,1,0
+Hot Pink Leather Chestplate: 100,1,0
+Energy Drink: 100,1,0
+magic_Quantum Gemerald: 100,1,0
+Fortune Cookie: 100,1,0
+Custom Painting: 100,1,0
+Written Book: 100,1,0
+file_head_herobrine: 100,1,0
+
+#
+# Generic configuration not relevent to the example
+#
+[halls]
+Single: 20
+Double: 20
+Triple: 10
+Four: 10
+Ten: 10
+[rooms]
+Basic: 240
+Basic2x2: 80
+Corridor: 240
+Circular: 240
+Pit: 40
+CircularPit: 40
+SandstoneCavern: 0
+SandstoneCavernLarge: 10
+NaturalCavern: 0
+NaturalCavernLarge: 10
+Cavern: 0
+CavernLarge: 10
+CellBlock: 40
+GreatHallNS: 30
+GreatHallEW: 30
+[floors]
+Blank: 20
+Cobble: 10
+BrokenCobble: 30
+WoodTile: 10
+MixedWoodTile: 10
+CheckerRug: 5
+BrokenCheckerRug: 15
+CheckerClay: 5
+BrokenCheckerClay: 15
+DoubleSlab: 10
+BrokenDoubleSlab: 30
+Mud: 30
+Sand: 30
+StoneBrick: 10
+BrokenStoneBrick: 30
+[features]
+Arcane: 10
+Blank: 10
+Cell: 10
+Chapel: 10
+CircleOfSkulls: 5
+ConstructionArea: 10
+Columns: 30
+Chasm: 10
+Dais: 10
+Farm: 10
+Forge: 10
+LavaChasm: 10
+Mushrooms: 10
+Pool: 10
+River: 10
+MessHall: 10
+[stairwells]
+Scaffolding: 10
+Stairwell: 10
+TripleStairs: 10
+TowerWithLadder: 10
+[secret rooms]
+SecretAlchemyLab: 10
+SecretArmory: 10
+SecretEnchantingLibrary: 10
+SecretSepulchure: 10
+SecretStudy: 10
+[ruins]
+Blank: 40
+HouseFrame: 10
+CircularTower: 20
+SquareTower: 20
+Arches: 10
+[treasure rooms]
+Arena: 10
+Crypt: 30
+PitWithArchers: 10
+ThroneRoom: 30
+SpiderLair: 10
+EndPortal: 20

commit 71a84d782d56a2b14e5e505d19248ef062b8f473

Provide default for mapsorte paintings.
Fixes crash caused by bf69e75

mapstore.py
@@ -25,7 +25,7 @@ from utils import Vec
 import materials
 
 class new:
-    def __init__(self, mapstore, dir_paintings):
+    def __init__(self, mapstore, dir_paintings='paintings'):
         self.mapstore = os.path.join(mapstore, 'data')
 
         # Load the idcounts.dat NBT if it exists, otherwise make