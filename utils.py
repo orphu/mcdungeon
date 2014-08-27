6319f02336007dd1651449bc258fe2fcbb0b8a44
Steve Shipway <steve@steveshipway.org>
2014-08-27 16:54:51 +1200

    Revert accidental reversion of decodeDungeonInfo

--- utils.py
+++ utils.py
@@ -947,16 +947,7 @@ def decodeDungeonInfo(lib):
     # iterate through the objects in the chest
     for book in lib['Items']:
         if (
-            (book['id'] != 387 and 'minecraft:written_book' not in book['id'])
-        ):
-            #print 'Non-book found in cache chest: %s' % ( book['id'] )
-            continue
-        if (
-			'title' not in book['tag']
-        ):
-            print 'Strange book with no title found in cache chest' 
-            continue
-        if (
+            (book['id'] != 387 and book['id'] != 'minecraft:written_book' ) or
             book['tag']['title'].startswith('MCDungeon Data Volume') is False
         ):
             print 'Non-book or odd book found in chest!', items['position'], 'id:', book['id']
