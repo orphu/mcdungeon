9f90bf88f701884efc461abe9fc4d14e754cc9f5
JiFish <jifish0@gmail.com>
2014-09-14 03:25:04 +0100

    Add convert to ordinal utility function

--- utils.py
+++ utils.py
@@ -1590,6 +1590,37 @@ def get_entity_other_tags(eid='EnderCrystal', Direction='S',
         root_tag['Motive'] = nbt.TAG_String(Motive)
 
     return root_tag
+    
+
+# Convert number to ordinal (1st, 2nd etc.)
+# source: http://stackoverflow.com/questions/9647202/ordinal-numbers-replacement
+def converttoordinal(numb):
+    if numb < 20: #determining suffix for < 20
+        if numb == 1: 
+            suffix = 'st'
+        elif numb == 2:
+            suffix = 'nd'
+        elif numb == 3:
+            suffix = 'rd'
+        else:
+            suffix = 'th'  
+    else:   #determining suffix for > 20
+        tens = str(numb)
+        tens = tens[-2]
+        unit = str(numb)
+        unit = unit[-1]
+        if tens == "1":
+           suffix = "th"
+        else:
+            if unit == "1": 
+                suffix = 'st'
+            elif unit == "2":
+                suffix = 'nd'
+            elif unit == "3":
+                suffix = 'rd'
+            else:
+                suffix = 'th'
+    return str(numb)+ suffix
 
 
 def DebugBreakpoint(banner="Debugger started (CTRL-D to quit)"):
