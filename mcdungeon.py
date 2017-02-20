3777d239259f393a2daab035b5240396206768d4
orphu <github@bubblemod.org>
2017-02-20 01:55:36 +0000

    Verison bump

--- mcdungeon.py
+++ mcdungeon.py
@@ -15,7 +15,7 @@ if platform.system() != 'Windows':
     import concurrent.futures as cf
 
 # Version info
-__version__ = '0.17.1'
+__version__ = '0.18.0'
 __version_info__ = tuple([num for num in __version__.split('.')])
 _vstring = '%%(prog)s %s' % (__version__)
 
